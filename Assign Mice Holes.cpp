d005603906393c7cd2bbcddac2fa69f08b2a96f0
Akshat Raj <139798410+akshatrsingh21@users.noreply.github.com>
2025-09-09 22:56:41 +0530

    Create Assign Mice Holes.cpp

---
+++ Assign Mice Holes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int assignHole(vector<int>& mices, vector<int>& holes) {
+        int ans = INT_MIN;
+        sort(mices.begin(), mices.end());
+        sort(holes.begin(), holes.end());
+        int n = mices.size();
+        for(int i=0;i<n;i++){
+            ans = max(ans, abs(holes[i]-mices[i]));
+        }
+        return ans;
+    }
+};
