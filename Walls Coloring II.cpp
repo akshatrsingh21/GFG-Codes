8c1682b9187182ddea9a136e5ffb7c9b73de1cf6
Akshat Raj <139798410+akshatrsingh21@users.noreply.github.com>
2025-12-05 22:30:24 +0530

    Create Walls Coloring II.cpp

---
+++ Walls Coloring II.cpp
@@ -0,0 +1,40 @@
+class Solution{
+public:
+    int minCost(vector<vector<int>> &costs) {
+        int n = costs.size();
+        int k = costs[0].size();
+        
+        int dp[n][k];
+        set<pair<int,int>> x, y;
+        
+        for(int i = 0; i < k; i++){
+            dp[0][i] = costs[0][i];
+            x.insert({dp[0][i], i});
+        }
+            
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < k; j++){
+                dp[i][j] = 1e9;
+                
+                auto ptr = x.begin();
+                if(*ptr == make_pair(dp[i - 1][j], j))
+                    ++ptr;
+            
+                if(ptr != x.end())
+                    dp[i][j] = costs[i][j] + (*ptr).first;
+                
+                y.insert({dp[i][j], j});
+            }
+            
+            x = y;
+            y.clear();
+        }
+        
+        int ans = 1e9;
+        
+        for(int i = 0; i < k; i++)
+            ans = min(ans, dp[n - 1][i]);
+            
+        return ((ans == 1e9) ? -1 : ans);
+    }
+};
